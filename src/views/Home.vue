a90670fdfa953a0ede5e7f4458889c18a9d37115
mikik0 <usapon_love1222@yahoo.co.jp>
2021-12-14 13:22:38 +0900

    routeの設定

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Task App !</h1>
+    <router-link to="/tasks">start</router-link>
+    <h1>{{ message }}</h1>
+    <li v-for = "(value, key) in columns" :key="key">
+    {{ user.name }}:<button v-on:click="deleteUser(user.id)">削除</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+    data(){
+        return {
+            users: [],
+            name: ''
+        }
+    },
+    methods: {
+        getProducts () {
+            this.axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                this.products = response.data
+            })
+        }
+    }
+}
+</script>
---
+++ src/views/Tasks.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Task</h1>
+    <v-row>
+      <v-col cols="4">
+      <form>
+        <v-text-field
+          v-model="task.title"
+          label="Title"
+        ></v-text-field>
+        <v-textarea
+          v-model="task.content"
+          label="Content"
+        ></v-textarea>
+
+        <v-btn class="mr-4" @click="onSubmit">Create</v-btn>
+      </form>
+      </v-col>
+      <v-col cols="8">
+        <table>
+          <tr>
+            <th>Title</th>
+            <th>Content</th>
+          </tr>
+          <tr>
+            <td>show</td>
+            <td>Edit</td>
+            <td>Destroy</td>
+          </tr>
+        </table>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: {}
+    }
+  },
+  methods: {
+    onSubmit() {
+    }
+  }
+}
+</script> 
