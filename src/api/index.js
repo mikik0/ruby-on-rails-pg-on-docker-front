d32f1eded50d185089ca3457cde461fc5ec545e5
mikik0 <usapon_love1222@yahoo.co.jp>
2021-12-15 16:32:38 +0900

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-export default () => {
-  return axios.create({
+export const axiosBase = axios.create({
     baseURL: 'http://localhost'
   })
-}
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -2,26 +2,26 @@
   <div>
     <h1>Task</h1>
     <v-row>
-    <table>
-    <thead>
-        <tr>
-        <th>title</th>
-        <th>content</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="task in tasks" :key="task.id">
-        <td>{{ task.title }}</td>
-        <td>{{ task.content }}</td>
-        </tr>
-    </tbody>
-    </table>
+      <table>
+        <thead>
+            <tr>
+              <th>title</th>
+              <th>content</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr v-for="task in tasks" :key="task.id">
+            <td>{{ task.title }}</td>
+            <td>{{ task.content }}</td>
+          </tr>
+        </tbody>
+      </table>
     </v-row>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import {axiosBase} from '../api/index'
 export default {
   name: 'Tasks',
   data () {
@@ -30,16 +30,12 @@ export default {
     }
   },
   mounted () {
-    axios.get('http://localhost/tasks')
+    axiosBase.get('/tasks')
       .then(response => { 
         response.data.forEach(task => {
           this.tasks.push(task)
         })
-        console.log(response.data)
       })
-      .catch((error)=>{
-      console.log(error)
-    })
   }
 }
 </script>
