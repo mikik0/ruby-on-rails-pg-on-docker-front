e492e875f36cbb1b338d437b75520c4b9380b6e8
mikik0 <usapon_love1222@yahoo.co.jp>
2021-12-15 13:24:52 +0900

    いらない文章の削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,5 @@
   <div class="home">
     <h1>Welcome to Task App !</h1>
     <router-link to="/tasks">start</router-link>
-    <h1>{{ message }}</h1>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'Home',
-  data () {
-    return {
-      message: 'Welcome to Your Vue.js App',
-    }
-  }
-}
-</script>
